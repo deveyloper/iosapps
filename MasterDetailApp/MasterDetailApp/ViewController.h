e73479c7027b97ad43ce97568a01e0112702ba41
fatih <fatih@fatihs-Mac.local>
2014-01-11 23:20:21 +0200

    project created

---
+++ MasterDetailApp/MasterDetailApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MasterDetailApp
+//
+//  Created by fatih on 11/01/14.
+//  Copyright (c) 2014 fatih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
